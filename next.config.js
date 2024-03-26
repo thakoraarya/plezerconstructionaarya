88d4c98e20941557c7012fb907edf403cea5462b
Vidit Pujara <viditpujara@outlook.com>
2024-03-26 17:11:35 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  
+ 
+  images: { unoptimized: true },
+
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.(pdf)$/,
